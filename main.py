64f10522b2e04577c6676770ae149785e7d128d5
Christopher Müller <christopher.mueller@bnewable.de>
2025-09-04 13:29:44 +0200

    fix: keep main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "uv-audit @ git+https://github.com/SpielerNogard/uv-audit.git@main"
+# ]
+# ///
+import warnings
+
+if __name__ == "__main__":
+    from uv_audit import app
+
+    warnings.warn(
+        category=DeprecationWarning,
+        message="This script is deprecated. Please use the 'uv-audit' command directly.",
+    )
+    app()
